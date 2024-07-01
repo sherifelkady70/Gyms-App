80d919cc689ae79db4cbce7e8cdabcc8e87fd3f8
sherif Abdelrhman <sellkady038@gmail.com>
2024-07-01 23:21:13 +0300

    finish state handle object to save state of list when system kill process by making two functions one for save id for item that isFav is true and one for restore list with fav items.

--- app/src/main/java/com/route/gyms_app/GymsViewModel.kt
+++ app/src/main/java/com/route/gyms_app/GymsViewModel.kt
@@ -1,13 +1,14 @@
 package com.route.gyms_app
 
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.toMutableStateList
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 
 class GymsViewModel(
     private val stateHandle:SavedStateHandle
 ) : ViewModel() {
-    var state = mutableStateOf(getListOfGyms())
+    var state = mutableStateOf(restoreGymsListData())
     private fun getListOfGyms() : List<GymModel> {
         return Gyms.listOfGyms
     }
@@ -16,6 +17,28 @@ class GymsViewModel(
         val gyms = state.value.toMutableList()
         val itemIndex = gyms.indexOfFirst { it.id==gymId }
         gyms[itemIndex] = gyms[itemIndex].copy(isFavorite = !gyms[itemIndex].isFavorite)
+        saveGymsListData(gyms[itemIndex])
         state.value = gyms
     }
+
+    private fun saveGymsListData(gym:GymModel){
+        val stateHandleList = stateHandle.get<List<Int>>(FAV_KEY).orEmpty().toMutableStateList()
+        if(gym.isFavorite) stateHandleList.add(gym.id)
+        else stateHandleList.remove(gym.id)
+        stateHandle[FAV_KEY] = stateHandleList
+    }
+
+    private fun restoreGymsListData():List<GymModel>{
+        val gyms = getListOfGyms()
+       stateHandle.get<List<Int>>(FAV_KEY)?.forEach { savedIds ->
+           savedIds.let {
+               gyms.find { it.id== savedIds }?.isFavorite=true
+           }
+       }
+        return gyms
+    }
+    companion object{
+        const val FAV_KEY = "favorite_key"
+    }
 }
+
